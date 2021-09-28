93d296cbafc8914e3363c801210d014721003936
R-Dmitrii <0991991@mail.ru>
2021-09-28 14:36:18 +0300

    Практическое задание №3

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+ import java.util.ArrayList;
+ import java.util.Arrays;
+
+public class Main {
+
+    static <T> void swap(T[] array, int firstIndex, int secondIndex) {
+        T oneVal = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = oneVal;
+    }
+
+    static void swapObj(Object[] array, int firstIndex, int secondIndex) {
+        Object oneVal = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = oneVal;
+    }
+
+    static <T> ArrayList<T> convertToList(T[] array) {
+        return new ArrayList<>(Arrays.asList(array));
+    }
+}
