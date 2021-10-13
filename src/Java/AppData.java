2840d8f8e21af14ce4d513dc2b332a334aa0aae7
R-Dmitrii <0991991@mail.ru>
2021-10-13 18:43:12 +0300

    Практическое задание №5

---
+++ src/Java/AppData.java
@@ -0,0 +1,19 @@
+
+
+    public class AppData {
+         private String[] header;
+         private int[][] data;
+
+    public void setData(String[] header, int[][] data) {
+          this.header = header;
+           this.data = data;
+}
+
+      public String[] getHeader() {
+      return header;
+}
+
+    public int[][] getData() {
+      return data;
+  }
+}
