2dbfa8d6b3c8464841f0ff530805b4db7627c789
R-Dmitrii <0991991@mail.ru>
2021-10-18 22:57:37 +0300

    Практическое задание №7

---
+++ src/Lesson_7/dto/Main.java
@@ -0,0 +1,24 @@
+package Lesson_7.dto;
+
+
+        import com.fasterxml.jackson.annotation.JsonIgnore;
+        import lombok.Getter;
+        import lombok.Setter;
+
+@Getter
+@Setter
+public class Main {
+    float temp;
+    float feels_like;
+    float temp_min;
+    float temp_max;
+    float pressure;
+    int sea_level;
+    int grnd_level;
+    int humidity;
+    float temp_kf;
+
+    public float getTemp(){
+        return temp-273.15f;
+    }
+}
---
+++ src/Lesson_7/dto/Sys.java
@@ -0,0 +1,11 @@
+package Lesson_7.dto;
+
+
+        import lombok.Getter;
+        import lombok.Setter;
+
+@Getter
+@Setter
+public class Sys {
+    String pod;
+}
